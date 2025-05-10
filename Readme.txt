6d6bb744a05ed96e75138ae1b1285e87d033ee48
Pradeepa Lakruwan <pradeepa@creatit.com.au>
2025-05-10 10:48:38 +0530

    Used snake case deep func for data integrate.

--- Readme.txt
+++ Readme.txt
@@ -9,3 +9,29 @@ npx react-native run-ios
 
 //For android
 npx react-native run-android
+npx react-native log-android       
+
+
+//get ip address - mac
+ipconfig getifaddr en0
+
+//packages
+yarn add 
+@react-navigation/native 
+@react-navigation/native-stack 
+react-native-screens 
+react-native-safe-area-context 
+react-native-gesture-handler 
+@react-navigation/bottom-tabs 
+@reduxjs/toolkit 
+react-redux 
+redux-persist 
+@react-native-async-storage/async-storage 
+axios 
+react-native-mmkv 
+react-native-vector-icons 
+react-native-svg
+react-hook-form zod @hookform/resolvers
+-D @types/lodash
+@react-native-picker/picker
+
--- src/core/api/axios.ts
+++ src/core/api/axios.ts
@@ -1,8 +1,24 @@
 import axios from 'axios';
 import {store} from '../store/store';
 import type {RootState} from '../store/store';
+import {mapKeys, snakeCase, isPlainObject, isArray} from 'lodash';
 
-const api = axios.create({baseURL: 'http://192.168.8.102:8000/api/'});
+// Utility to deeply convert camelCase to snake_case
+const toSnakeCaseDeep = (obj: any): any => {
+  if (isArray(obj)) {
+    return obj.map(toSnakeCaseDeep);
+  } else if (isPlainObject(obj)) {
+    const converted: any = {};
+    Object.keys(obj).forEach(key => {
+      const newKey = snakeCase(key);
+      converted[newKey] = toSnakeCaseDeep(obj[key]);
+    });
+    return converted;
+  }
+  return obj;
+};
+
+const api = axios.create({baseURL: 'http://192.168.8.105:8000/api/'});
 
 api.interceptors.request.use(config => {
   const state = store.getState() as RootState;
@@ -11,6 +27,10 @@ api.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   config.headers['x-api-key'] = '123';
+
+  if (config.data) {
+    config.data = toSnakeCaseDeep(config.data);
+  }
   return config;
 });
 
